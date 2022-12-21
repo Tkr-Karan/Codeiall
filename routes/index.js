3d3d08f341578bffa1a14ed901015ba64b608f8c
Karan Kumar <karankumar072508@gmail.com>
2022-12-21 23:40:04 +0530

    Adding controller
    here we are adding different controller in a separate files and folder.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+// adding controller here.
+const homeController = require('../controllers/home_controller');
 
 console.log("routes added successfully!!");
+
+
+// adding controller for our page.
+router.use('/', homeController.home);
+
+router.use('/login', homeController.login);
 
 
 module.exports = router;
