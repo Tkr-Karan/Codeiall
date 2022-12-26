8fab14063f64de3429bb0a42a04359ae2ffa6176
Karan Kumar <karankumar072508@gmail.com>
2022-12-27 00:18:53 +0530

    Rendering pages for sign in and sign up

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,3 +4,20 @@ module.exports.profile = function(req, res) {
         title: "user Page"
     });
 }
+
+
+// render the Sigin page
+module.exports.signIn = function(req, res) {
+
+    return res.render('user_sign_in', {
+        title: "Codiall | Signin"
+    })
+}
+
+
+// render the Sign Up Page
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: "Codeiall | SignUp"
+    })
+}
--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,9 @@ console.log('user controller add successfully!!!!');
 
 router.get('/profile', profile_controller.profile);
 
-
+// connecting the user Sig In and Sign up routes to the conttroller.
+router.get('/sign-in', profile_controller.signIn);
+router.get('/sign-up', profile_controller.signUp);
 
 
 module.exports = router;
