7679efc472454643bbe544628f2bdc7a41c7fe74
Karan Kumar <karankumar072508@gmail.com>
2022-12-27 00:07:11 +0530

    Setting up the User Schema.

---
+++ models/user.js
@@ -0,0 +1,28 @@
+// here we create the schema for the user.
+const mongoose = require('mongoose');
+
+//creating user fields collections
+const userSchema = new mongoose.Schema({
+    email : {
+        type: String,
+        required: true,
+        unique: true // here unique gives the unique identity to the email.
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true // here timestamps gives us the property or instance in tableof CreatedAt and UpdatedAt.
+})
+
+
+//creating an Schema which is visible in the db.
+const User = mongoose.model('User', userSchema);
+
+// exporting the modules
+module.exports = User;
