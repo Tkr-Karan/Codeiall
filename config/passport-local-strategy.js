e392ab0767bfc1ff26f36c5b2b8e8924b5c444f9
Karan Kumar <karankumar072508@gmail.com>
2023-01-07 23:09:24 +0530

    checking

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,56 @@
+// importing the passport using require
+const passport = require('passport');
+
+//creating passport strategy variable
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+
+// creating the passport middleware
+
+//authentication using passport
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+    },
+    // here we using the callback function
+    function(email, password, done){  // done is the callback function
+        // find the user and estabish the identity
+        User.findOne({email: email}, function(err, user){
+            if(err) {
+                console.log("error in finding user --> Passport");
+                return done(err);
+            }
+
+            if(!user || user.password != password){
+                console.log("Invalid Username/Password");
+                return done(null, false);
+            }
+
+            return done(null, user);
+        });
+    }
+));
+
+// serializing the user to decide which key is kept in the cookies.
+passport.serializeUser(function(user, done){
+    console.log("You are here");
+    return done(null, user.id);
+});
+
+
+
+// deserializing the user from the key in the cookies 
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        if(err){
+            console.log("there is an errror --> Passport");
+            return done(err);
+        }
+
+        return done(null, user);
+    });  
+});
+
+// here we are exporting the module
+module.exports = passport;
--- routes/user.js
+++ routes/user.js
@@ -22,9 +22,10 @@ router.post('/create', profile_controller.create);
 
 // here we create the router for the create-session
 // here our roter takes the three arguemnt instead of two and one is the middleware.
-router.post('create-session', passport.authenticate(
+router.post('/create-session', passport.authenticate(
     'local',
-    {failureRedirect: 'user/sign-in'}
+    // console.log("You are stuck here"),
+    {failureRedirect: '/user/sign-in'}
 ) , profile_controller.createSession);
 
 
