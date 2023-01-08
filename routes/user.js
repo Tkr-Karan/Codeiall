076faf51bf7c94c4394ea068e4ff12efc3dd1df6
Karan Kumar <karankumar072508@gmail.com>
2023-01-08 23:57:28 +0530

    checking

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,8 @@ const profile_controller = require('../controllers/user_controller');
 
 console.log('user controller add successfully!!!!');
 
-
-router.get('/profile', profile_controller.profile);
+ 
+router.get('/profiles', profile_controller.profile);
 
 // connecting the user Sig In and Sign up routes to the conttroller.
 router.get('/sign-in', profile_controller.signIn);
